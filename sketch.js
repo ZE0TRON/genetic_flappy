80aeaed6c3169787a452e2606111bf120152d764
ze0tron <cmpbilge@gmail.com>
2019-04-17 10:33:10 +0300

    training mode added, gamespeed slider added, show birds/pipes is optional now

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
 let birds = [];
 let old_gen = [];
 var pipes = [];
-const NUMBER_OF_BIRDS = 500;
+const NUMBER_OF_BIRDS = 3000;
+var bestBird ;
+var showBirds = true;
+var showPipes = true;
 var score;
 var highScore;
 var generation = 0;
+var gameSpeed = 60;
+var isTraining = false;
 function setup() {
   score = 0;
   highScore = 0;
@@ -13,24 +18,38 @@ function setup() {
     birds.push(new Bird());
   }
   pipes.push(new Pipe());
+  playBestButton = createButton('Play the best bird so far');
+  playBestButton.position(5, 405);
+  playBestButton.mousePressed(playBestBird);
+  showBirdsButton =  createButton("Show Birds");
+  showPipesButton = createButton("Show Pipes");
+  showBirdsButton.position(5,430);
+  showPipesButton.position(5,450);
+  showBirdsButton.mousePressed(()=> {showBirds = !showBirds});
+  
+  showPipesButton.mousePressed(()=> {showPipes = !showPipes});
+  speedSlider = createSlider(1, 100, 1);
+  speedSlider.position(100,480);
+  trainButton = createButton("Train");
+  trainButton.position(5,480);
+  trainButton.mousePressed(()=> {isTraining = !isTraining; train()});
 }
 
-function draw() {
-  background(0);
+function gameLoop(){
   calculateFitness();
   score+=1;
-  if(frameCount % 70 == 0){
+  if(score % 70 == 0){
     pipes.push(new Pipe());
   }
 
   for(pipe of pipes){
     pipe.update();
-    pipe.show();
     for(bird of birds){
       if(pipe.hits(bird)) {
         old_gen.push(birds.splice(birds.indexOf(bird),1)[0]);
        
         if(birds.length == 0){
+          bestBird = old_gen[old_gen.length-1];
          restart();
          return;
         }
@@ -39,17 +58,73 @@ function draw() {
     if(pipe.offscreen()){
       pipes.splice(pipes.indexOf(pipe),1);
     }
-
-    let s = `Score  : ${score}\nHighScore : ${highScore}\nGeneration : ${generation}`;
-    fill(255);
-    text(s, 10, 10, 100, 80); // Text wraps within text box
+    // Text wraps within text box
   }
   for(bird of birds)  {
     bird.score+=1;
     bird.think(pipes);
     bird.update();
-    bird.show();
   }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function train(){
+  while(isTraining){
+    for(let i =0 ; i<gameSpeed; i++){
+      gameLoop();
+    }
+    await sleep(1);
+  }
+}
+
+function draw() {
+  background(0);
+  if(!isTraining){
+    calculateFitness();
+    gameSpeed = speedSlider.value();
+    score+=1;
+    if(frameCount % 70 == 0){
+      pipes.push(new Pipe());
+    }
+
+    for(pipe of pipes){
+      pipe.update();
+      if(showPipes){
+        pipe.show();
+      }
+      for(bird of birds){
+        if(pipe.hits(bird)) {
+          old_gen.push(birds.splice(birds.indexOf(bird),1)[0]);
+        
+          if(birds.length == 0){
+            bestBird = old_gen[old_gen.length-1];
+          restart();
+          return;
+          }
+        }
+    }
+      if(pipe.offscreen()){
+        pipes.splice(pipes.indexOf(pipe),1);
+      }
+
+      // Text wraps within text box
+    }
+    for(bird of birds)  {
+      bird.score+=1;
+      bird.think(pipes);
+      bird.update();
+      if(showBirds){
+        bird.show();
+      }
+    }
+  }
+  let s = `Score  : ${score}\nHighScore : ${highScore}\nGeneration : ${generation}`;
+  //fill(255);
+  text(s, 10, 10, 100, 80);
+  let speedText = `Speed  : x${gameSpeed}`;
+  text(speedText, 500, 350, 100, 80);
 }
 function keyPressed() {
   if(key == ' '){
@@ -68,4 +143,11 @@ function restart() {
   }
   score = 0;
   pipes.push(new Pipe()); 
+}
+
+function playBestBird() {
+  for(bird of birds) {
+    old_gen.push(bird);
+  }
+  birds = [bestBird];
 }
